118202f9fa59f17aa7996ad899b5ec1b4d536f55
Rodrigo P Maruyama <rodmaru@gmail.com>
2018-06-14 12:14:05 -0300

    dataAnalysisR folder created. file lesson4 created.

---
+++ dataAnalysisR/lesson4.R
@@ -0,0 +1,27 @@
+# Lesson 4: Explore One Variable
+
+install.packages('ggplot2')
+library('ggplot2')
+diamonds
+
+# Quiz 1: Diamonds
+nrow(diamonds)
+names(diamonds)
+levels(diamonds)
+diamonds$color
+
+# Quiz 2: Price Histogram
+qplot(diamonds$price, geom="histogram", bins="50")
+
+# Quiz 3: Price Histogram Summary
+median(diamonds$price)
+mean(diamonds$price)
+
+# Quiz 4: Diamond counts
+subset(diamonds, price < 500)
+subset(diamonds, price < 250)
+subset(diamonds, price >= 15000)
+
+
+qplot(diamonds$price, geom="histogram", bins="50")
+
