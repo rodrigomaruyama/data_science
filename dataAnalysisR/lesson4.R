4348f7d4c49cba158d88d186bc62e35f394b5884
Rodrigo P Maruyama <rodmaru@gmail.com>
2018-06-15 14:29:52 -0300

    Question 8. free_y added to the qplot command.

--- dataAnalysisR/lesson4.R
+++ dataAnalysisR/lesson4.R
@@ -26,5 +26,20 @@ subset(diamonds, price < 250)
 subset(diamonds, price >= 15000)
 
 # Quiz 5: Cheaper Diamonds
-qplot(diamonds$price, geom="histogram", bins="50")
-ggsave('dsNanodegree/machine-learning-master/projects/finding_donors/priceAnalysisR/priceHistogram.jpg')
+qplot(diamonds$price, geom="histogram") + xlim(c(250, 1500))
+ggsave('C:/Users/maru/Documents/dsNanodegree/machine-learning-master/projects/finding_donors/dataAnalysisR/priceHistogram.jpg')
+
+# Quiz 6: Price by Cut Histograms
+# https://www.tutorialgateway.org/r-ggplot2-histogram/
+ggplot(data = diamonds, aes(x = price, fill = cut)) + 
+	geom_histogram(binwidth = 100) +
+	facet_wrap(~ cut)
+
+# Quiz 7: Price by cut
+# https://discussions.udacity.com/t/problem-set-3-price-by-cut/128116
+by(diamonds$price,diamonds$cut,max)
+by(diamonds$price,diamonds$cut,min)
+by(diamonds$price,diamonds$cut,median)
+
+# Quiz 8: Scales and Multiple Histograms
+qplot(x = price, data = diamonds) + facet_wrap(~cut, scales = "free_y")
