1ff5ac2cb1839d3fa4822246a84e755bd1660872
Rodrigo P Maruyama <rodmaru@gmail.com>
2018-08-14 06:16:30 -0300

    directories removed

--- fdsc1/week1/Semana1-Lesson3-9.py
+++
@@ -1,7 +0,0 @@
-# Write python code that defines the variable
-# age to be your age in years, and then prints
-# out the number of days you have been alive.
-
-age = 42
-number_of_days = 42*365
-print number_of_days
